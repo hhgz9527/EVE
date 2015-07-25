eab66324edfe5c7e047e7aa1efb270a753ada82e
Du Yizhuo <dyzdyz010@sina.com>
2015-07-25 21:36:24 +0800

    Add CREST token

---
+++ EVE/EVE/CRest.swift
@@ -0,0 +1,17 @@
+//
+//  CRest.swift
+//  EVE
+//
+//  Created by 杜艺卓 on 15/7/25.
+//  Copyright © 2015年 MoShou. All rights reserved.
+//
+
+import Foundation
+
+
+class CRest {
+    
+    let REFRESH_TOKEN = "93ro-UYn0v6RGTvcS6PQwYqEkfGCBw2gmhbsm93c9WY1"
+    
+    var access_token = ""
+}
