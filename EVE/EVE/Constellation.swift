dae4d6a3ae1b1ad2867898da64425b6e92791d49
Du Yizhuo <dyzdyz010@sina.com>
2015-07-30 21:10:55 +0800

    Home page in progress

---
+++ EVE/EVE/Constellation.swift
@@ -0,0 +1,16 @@
+//
+//  Constellation.swift
+//  EVE
+//
+//  Created by 杜艺卓 on 15/7/29.
+//  Copyright © 2015年 MoShou. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Constellation {
+    
+    static let virgoPoints: Array<(CGFloat, CGFloat)> = [(30, 5), (137, 81), (184, 151), (275, 178), (343, 103), (10, 85), (76, 135), (52, 178), (134, 245), (266, 260), (299, 312), (388, 340)]
+    static let virgoLines: Array<(Int, Int)> = [(0, 1), (1, 2), (2, 3), (3, 4), (5, 6), (6, 7), (7, 8), (8, 9), (9, 10), (10, 11), (2, 8), (3, 9)]
+}
